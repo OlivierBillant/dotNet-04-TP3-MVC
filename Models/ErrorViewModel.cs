744ad1a2be46b0d224f49392484a99de027d8f01
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-08-24 10:08:24 +0200

    CREATE

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet._04.TP3.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
