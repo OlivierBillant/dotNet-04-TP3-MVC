95aa0d7752530b376c3bc3c759f2dcc502976537
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-08-24 12:10:26 +0200

    EDIT : minor

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,89 @@
+﻿namespace DotNet._04.TP3.MVC.Controllers;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DotNet._04.TP3.MVC.Models;
+
+public class ChatController : Controller
+{
+    private static List <ChatViewModel> meuteDeChats = ChatViewModel.GetMeuteDeChats();
+    // GET: ChatController
+    public ActionResult Index()
+    {
+        return View(meuteDeChats);
+    }
+
+    // GET: ChatController/Details/5
+    public ActionResult Details(int id)
+    {
+        // var meuteDeChats = ChatViewModel.GetMeuteDeChats();
+
+        return this.View(meuteDeChats.Where(chat => chat.Id == id).FirstOrDefault());
+    }
+
+    /* // GET: ChatController/Create
+     public ActionResult Create()
+     {
+         return View();
+     }
+
+     // POST: ChatController/Create
+     [HttpPost]
+     [ValidateAntiForgeryToken]
+     public ActionResult Create(IFormCollection collection)
+     {
+         try
+         {
+             return RedirectToAction(nameof(Index));
+         }
+         catch
+         {
+             return View();
+         }
+     }
+
+     // GET: ChatController/Edit/5
+     public ActionResult Edit(int id)
+     {
+         return View();
+     }
+
+     // POST: ChatController/Edit/5
+     [HttpPost]
+     [ValidateAntiForgeryToken]
+     public ActionResult Edit(int id, IFormCollection collection)
+     {
+         try
+         {
+             return RedirectToAction(nameof(Index));
+         }
+         catch
+         {
+             return View();
+         }
+     }
+*/
+    // GET: ChatController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        // var meuteDeChats = ChatViewModel.GetMeuteDeChats();
+        return this.View(meuteDeChats.Where(chat => chat.Id == id).FirstOrDefault());
+    }
+
+    // POST: ChatController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            // var meuteDeChats = ChatViewModel.GetMeuteDeChats();
+            meuteDeChats.Remove(meuteDeChats.FirstOrDefault(chat => chat.Id == id));
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ChatViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNet._04.TP3.MVC.Models
+{
+    public class ChatViewModel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public string Couleur { get; set; }
+
+        public static List<ChatViewModel> GetMeuteDeChats()
+        {
+            var i = 1;
+            return new List<ChatViewModel>
+            {
+                new ChatViewModel{Id=i++,Nom = "Felix",Age = 3,Couleur = "Roux"},
+                new ChatViewModel{Id=i++,Nom = "Minette",Age = 1,Couleur = "Noire"},
+                new ChatViewModel{Id=i++,Nom = "Miss",Age = 10,Couleur = "Blanche"},
+                new ChatViewModel{Id=i++,Nom = "Garfield",Age = 6,Couleur = "Gris"},
+                new ChatViewModel{Id=i++,Nom = "Chatran",Age = 4,Couleur = "Fauve"},
+                new ChatViewModel{Id=i++,Nom = "Minou",Age = 2,Couleur = "Blanc"},
+                new ChatViewModel{Id=i,Nom = "Bichette",Age = 12,Couleur = "Rousse"}
+            };
+        }
+    }
+}
